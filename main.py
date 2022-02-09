b2f838fdfab084906448147b6b7efc13e5f70183
YEA <yea@omegasoftware.eu>
2022-02-09 18:51:55 +0300

    feat: add 'add' command

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ def start(message):
 @bot.message_handler(commands=['add'])
 def add(message):
     bot.send_message(message.chat.id, 'Example...')
+    bot.register_next_step_handler(message, check_message)
+
+def check_message(message):
+    if len(message.text.replace(" ", "").split(',')) != 3:
+        try:
+            valid_date = time.strptime(date, '%m/%d/%Y')
+        except ValueError:
+            print('Invalid date!')
+        bot.send_message(message.chat.id, 'bad request, try agin...')
+        bot.register_next_step_handler(message, check_message)
+    else:
+        bot.send_message(message.chat.id, 'okiokioki')
 
 
 bot.polling(none_stop=True, interval=0)
