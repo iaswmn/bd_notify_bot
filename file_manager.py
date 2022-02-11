f5719d6c795781eb8c8c86e00826830b270b06d3
YEA <yea@omegasoftware.eu>
2022-02-11 13:34:57 +0300

    feat: add new commands

---
+++ file_manager.py
@@ -0,0 +1,43 @@
+import json
+
+
+class FileManager:
+    def __init__(self):
+        self.file_name_json = 'bd_notify.json'
+        self.bd_data_column = "bd_data"
+
+    def save_data_to_file(self, data):
+        with open(self.file_name_json, 'r+') as file:
+            file_data = json.load(file)
+            file_data[self.bd_data_column].append(data)
+            file.seek(0)
+            json.dump(file_data, file, indent=4)
+
+    def get_data_by_id(self, from_id):
+        new_dict = []
+        with open(self.file_name_json, 'r') as file:
+            file_data = json.load(file)
+            for row in file_data[self.bd_data_column]:
+                if row['id'] == from_id:
+                    new_dict.append(row)
+        return new_dict
+
+    def update_data_by_id_and_i(self, from_id, row_number, new_data):
+        with open(self.file_name_json, 'r') as file:
+            file_data = json.load(file)
+            with open(self.file_name_json, 'w') as file:
+                for i in range(len(file_data[self.bd_data_column])):
+                    if file_data[self.bd_data_column][i]['id'] == from_id and i == row_number:
+                        file_data[self.bd_data_column][i]['name'] = new_data['name']
+                        file_data[self.bd_data_column][i]['date'] = new_data['date']
+                        file_data[self.bd_data_column][i]['nick'] = new_data['nick']
+                json.dump(file_data, file, indent=4)
+
+    def delete_data_by_id_and_i(self, from_id, row_number):
+        with open(self.file_name_json, 'r') as file:
+            file_data = json.load(file)
+            with open(self.file_name_json, 'w') as file:
+                for i in range(len(file_data[self.bd_data_column])):
+                    if file_data[self.bd_data_column][i]['id'] == from_id and i == row_number:
+                        del file_data[self.bd_data_column][i]
+                json.dump(file_data, file, indent=4)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import time
+
 import telebot
 from config import BotSettings
+from file_manager import FileManager
 
 bot = telebot.TeleBot(BotSettings.BOT_TOKEN.value)
+file_manager = FileManager()
 
 
 @bot.message_handler(commands=['start'])
@@ -11,19 +15,85 @@ def start(message):
 
 @bot.message_handler(commands=['add'])
 def add(message):
-    bot.send_message(message.chat.id, 'Example...')
-    bot.register_next_step_handler(message, check_message)
+    bot.send_message(message.chat.id, 'Example: Женя Программист, 01.01.1990, @test')
+    bot.register_next_step_handler(message, check_message, 2)
 
-def check_message(message):
-    if len(message.text.replace(" ", "").split(',')) != 3:
+
+@bot.message_handler(commands=['see'])
+def see(message):
+    show_all_bd_data_for_id(message.from_user.id)
+
+@bot.message_handler(commands=['cut'])
+def cut(message):
+    delete_row(message.from_user.id, message)
+
+@bot.message_handler(commands=['change'])
+def change(message):
+    bot.send_message(message.chat.id, 'Example: Женя Программист, 01.01.1990, @test, 2 (это номер записи из запроса see)')
+    bot.register_next_step_handler(message, check_message, 3)
+
+
+def extract_arg(arg):
+    return arg.split()[1:]
+
+
+def check_message(message, type):
+    data_list = message.text.replace(" ", "").split(',')
+    if len(data_list) >= 3:
         try:
-            valid_date = time.strptime(date, '%m/%d/%Y')
-        except ValueError:
-            print('Invalid date!')
-        bot.send_message(message.chat.id, 'bad request, try agin...')
-        bot.register_next_step_handler(message, check_message)
+            check_on_date(data_list[1])
+            check_on_nick(data_list[2])
+            prepare_data_to_save(message.from_user.id, data_list, type)
+        except Exception as e:
+            bot.send_message(message.chat.id, str(e))
+            bot.register_next_step_handler(message, check_message, type)
     else:
-        bot.send_message(message.chat.id, 'okiokioki')
+        bot.send_message(message.chat.id, 'bad request, try agin...')
+        bot.register_next_step_handler(message, check_message, type)
+
+
+def check_on_date(date_from_msg):
+    try:
+        time.strptime(date_from_msg, '%d.%m.%Y')
+    except ValueError:
+        raise Exception('Bad date format.')
+
+
+def check_on_nick(nick_from_msg):
+    if nick_from_msg[0] != '@':
+        raise Exception('Nick must start from @.')
+
+
+def prepare_data_to_save(from_id, data, save_type):
+    bd_dict = {'id': from_id,
+               'name': data[0],
+               'date': data[1],
+               'nick': data[2]}
+    if save_type == 2:
+        file_manager.save_data_to_file(bd_dict)
+        bot.send_message(from_id, "Added!")
+    elif save_type == 3:
+        update_row(from_id, bd_dict, data[3])
+
+
+def show_all_bd_data_for_id(from_id):
+    data = file_manager.get_data_by_id(from_id)
+    i = 0
+    for row in data:
+        message_to_send = f'''№{i} - {row['name']}, {row['date']}, {row['nick']}'''
+        bot.send_message(from_id, message_to_send)
+        i += 1
+
+
+def update_row(from_id, data, row_number):
+    file_manager.update_data_by_id_and_i(from_id, int(row_number), data)
+    bot.send_message(from_id, "Updated!")
+
+
+def delete_row(from_id, message):
+    args_dict = extract_arg(message.text)
+    file_manager.delete_data_by_id_and_i(from_id, int(args_dict[0]))
+    bot.send_message(from_id, "Deleted!")
 
 
 bot.polling(none_stop=True, interval=0)
